34082ab596c0e14ac0c5ec9831b919d0d87d8add
MaximeStl <max6440@protonmail.com>
2021-08-30 12:06:20 +0200

    premier test recuperer nombre et addition

--- calculatrice/src/com/MIP/calculatrice.java
+++ calculatrice/src/com/MIP/calculatrice.java
@@ -1,10 +1,20 @@
 package com.MIP;
+import java.util.Scanner;
 
 public class calculatrice {
-
+	static Scanner keyboard = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");
+		int numberOne = 0;
+		int numberTwo = 0;
+
+		System.out.println("Entrer le premier nombre");
+		numberOne = keyboard.nextInt();
+
+		System.out.println("Entrer le second nombre");
+		numberTwo = keyboard.nextInt();
+
+		System.out.println("La somme de " + numberOne + "et" + numberTwo + "est de :" + (numberOne + numberTwo));
 	}
 
 }
